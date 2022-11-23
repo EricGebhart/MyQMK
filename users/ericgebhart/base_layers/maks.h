f173cb9a2ed3731f8c23e00d07e704790a59d4ee
EricGebhart <e.a.gebhart@gmail.com>
2022-11-23 22:50:54 +0100

    Add Semimak and aptmak

--- users/ericgebhart/base_layers/maks.h
+++ users/ericgebhart/base_layers/maks.h
@@ -59,3 +59,60 @@
   LANG_MAP(_Q, _W, _D, _F, _K,   _Y, _U, _I,      _L,     _SCLN,        \
            _A, _S, _T, _R, _G,   _H, _N, _E,      _O,     _P,           \
            _Z, _X, _C, _V, _B,   _J, _M, TL_COMM, TL_DOT, TL_SLSH)
+
+
+#define CARTE_SEMIMAK_JQ                        \
+  carte_de_map( "  flhvz  'wuoy",               \
+                "  srntk  cdeai",               \
+                "  xjbmq  pg,./")
+
+#define ___SEMIMAK_JQ___                                                \
+  LANG_MAP(_F, _L, _H, _V, _Z,   TL_QUOT, _W, _U, _O, _Y,               \
+           _S, _R, _N, _T, _K,   _C, _D, _E, _A, _I,                    \
+           _X, _J, _B, _M, _Q,   _P, _G, TL_COMM, TL_DOT, TL_SLSH)
+
+#define CARTE_SEMIMAK                            \
+  carte_de_map( "  flhvz  qwuoy",                \
+                "  srntk  cdeai",                \
+                "  x'bmj  pg,./")
+
+#define ___SEMIMAK___                                                \
+  LANG_MAP(_F, _L, _H, _V, _Z,      _Q, _W, _U, _O, _Y,                 \
+           _S, _R, _N, _T, _K,      _C, _D, _E, _A, _I,                 \
+           _X, TL_QUOT, _B, _M, _J,  _P, _G, TL_COMM, TL_DOT, TL_SLSH)
+
+
+// APTMAP E on thumb.
+#define CARTE_APTMAK_30                         \
+  carte_de_map( "  ;wfpb  qluy'",               \
+                "  rsthk  jnaio",               \
+                "  xcgdv  zm,./  e")
+
+#define ___APTMAK_30___                                                 \
+  LANG_MAP(TL_SCLN, _W, _F, _P, _B,      _Q, _L, _U, _Y, TL_QUOT,       \
+           _R,      _S, _T, _H, _K,      _J, _N, _A, _I, _O,            \
+           _X,      _C, _D, _M, _V,      _P, _G, TL_COMM, TL_DOT, TL_SLSH)
+
+
+#define CARTE_APTMAK                            \
+  carte_de_map( "  /wfpb  jluy'",               \
+                "  rsthk  vnaio",               \
+                "  xcgdq  zm,.;  e")
+
+#define ___APTMAK___                                                    \
+  LANG_MAP(TL_SLSH, _W, _F, _P, _B,      _J, _L, _U, _Y, TL_QUOT,       \
+           _R,      _S, _T, _H, _K,      _V, _N, _A, _I, _O,            \
+           _X,      _C, _G, _D, _Q,      _Z, _M, TL_COMM, TL_DOT, TL_SCLN)
+
+// aptmak twist from @GNU on HD.
+// Needs combos for jqz.
+// repeat ⟳ key goes where SLSH is.
+#define CARTE_APTMAK_ALT                        \
+  carte_de_map( "  /wfpk  xluy'",               \
+                "  crstb  vnaio",               \
+                "   cgd    h,.   e")
+
+#define ___APTMAK_ALT___                                                \
+  LANG_MAP(TL_SLSH, _W, _F, _P, _K,      _X, _L, _U, _Y, TL_QUOT,       \
+           _C,      _R, _S, _T, _B,      _V, _N, _A, _I, _O,            \
+           _NO,      _C, _G, _D, _NO,      _NO, _H, TL_COMM, TL_DOT, _NO)
